22590c438364c32125f56f3d7bd7f6d6009f3b6e
rakesh membaram <gkmrakesh@gmail.com>
2016-10-27 20:26:01 +0530

    examples added

--- example.js
+++ example.js
@@ -7,3 +7,4 @@ console.log(arrayCount("")); // "invalid arguments"
 console.log(arrayCount([])); // 0
 console.log(arrayCount([1,2,3])); // 6
 console.log(arrayCount([1,2,"3"])); // 3
+console.log(arrayCount([1,2,"3",4])); // 7
